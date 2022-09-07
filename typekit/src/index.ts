c84e097b8779358206495e475be6cb9543336889
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-09-07 19:20:07 -0400

    Example with a TypeScript monorepo (#2)

--- typekit/src/index.ts
+++ typekit/src/index.ts
@@ -1,5 +1,37 @@
 import { solve } from "./buildkit.js";
 import { Image } from "./image.js";
-import { RunCargo, InstallRustToolchain } from "./commands.js";
+import { Command, Cargo, InstallRustToolchain } from "./commands.js";
+import {
+  Arg,
+  Cmd,
+  Copy,
+  Entrypoint,
+  Env,
+  Expose,
+  Label,
+  Run,
+  Shell,
+  User,
+  Volume,
+  WorkDir,
+} from "./instructions.js";
 
-export { Image, solve, RunCargo, InstallRustToolchain };
+export {
+  Arg,
+  Cmd,
+  Command,
+  Copy,
+  Entrypoint,
+  Env,
+  Expose,
+  Image,
+  InstallRustToolchain,
+  Label,
+  Run,
+  Cargo,
+  Shell,
+  User,
+  Volume,
+  WorkDir,
+  solve,
+};
